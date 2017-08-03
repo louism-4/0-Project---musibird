9b0fbc25f55b61aec33bc6ddb4e9fc570cd4fd3a
Louis Moc <louismc4@hotmail.com>
2017-08-03 17:51:14 -0600

    Add Files via FTT Algorithm

---
+++ Main.cs
@@ -0,0 +1,233 @@
+﻿using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using DenisVizigin.Objects3D;
+using UnityEngine;
+
+namespace DenisVizigin.Sound 
+{
+	public class Main : MonoBehaviour
+	{
+		public double RadiansToDegrees = 57.29577951308232;
+		public double CameraRotationEasing = 0.08;
+		public double PlayerMovingEasing = 0.1;
+		public float thresholdMultiplier = 1.5f;
+		public int thresholdSize = -1;
+		public double beatSensitivity = 1.5;
+		public int beatSubbands = 3;
+		public int sampleSize = 1024;
+		public int soundFeed = 100;
+		public int trackWidth = 1024;
+		public int trackHeight = 22;
+		public int trackDepth = 20000;
+		public int cameraDistanceY = 900;
+		public int cameraDistanceZ = 600;
+		//---
+	    
+		private MusicAnalyzer analyzer;
+		private AudioSource audio; 
+		private double targetPlayerZ = 0.0;
+		private double playerZ = 0.0;
+		private int lastSample = 0;
+	    private bool isAnalyzed = false;
+
+		WWW myClip; string mySongString;
+
+		public GameObject sphereRed;
+		public GameObject sphereOrange;
+		public GameObject sphereYellow;
+		public GameObject sphereGreen;
+		public GameObject sphereBlue;
+		public GameObject spherePurple;
+		public GameObject ring;
+
+		public GameObject laser;
+
+		public static GameObject slider;
+
+		public GameObject player; GameObject home; GameObject menu; public GameObject text; GameObject ready; GameObject loading;
+		GameObject coins; GameObject health;
+
+	    void Start()
+	    {
+			health = GameObject.Find ("Health");
+			health.SetActive (false);
+			coins = GameObject.Find ("Coins");
+			coins.SetActive (false);
+			text = GameObject.Find("Title");
+			mySongString = PlayerPrefs.GetString("song","");
+			if(PlayerPrefs.GetInt("playList",0) == 1){
+				text.GetComponent<Text>().text = mySongString;
+				Debug.Log (mySongString);
+				GetComponent<Camera>().GetComponent<AudioSource>().clip = (AudioClip)Resources.Load(mySongString);
+			} 
+			if (GetComponent<Camera>().GetComponent<AudioSource>().clip == null){
+				GetComponent<Camera>().GetComponent<AudioSource>().clip = (AudioClip)Resources.Load("Tobu - Such Fun");
+			} 
+			home = GameObject.Find("Home");
+			home.SetActive(false);
+			menu = GameObject.Find("Menu");
+			slider = GameObject.Find("LoadingSlider");
+			slider.SetActive(false);
+			Track.sphereRed = sphereRed;
+			Track.sphereOrange = sphereOrange;
+			Track.sphereYellow = sphereYellow;
+			Track.sphereGreen = sphereGreen;
+			Track.sphereBlue = sphereBlue;
+			Track.spherePurple = spherePurple;
+			Track.laser = laser; 
+			Track.ring = ring;
+			reInstantiate = false;
+			foundTrue = false;
+			executed = false;
+			isAnalyzed = false;
+	    	audio = GetComponent<Camera>().GetComponent<AudioSource>();
+	        analyzer = new MusicAnalyzer(GetComponent<Camera>().GetComponent<AudioSource>().clip, sampleSize, soundFeed, beatSubbands, beatSensitivity, 
+			                             thresholdSize, thresholdMultiplier);
+	    }
+		bool reInstantiate = false;
+		bool foundTrue = false;
+		bool executed = false;
+
+		float time;
+	    void Update()
+	    {
+			time += Time.deltaTime;
+			if(MusicHandler.startLoad){
+				if(GetComponent<AudioSource>().clip.isReadyToPlay && !GetComponent<AudioSource>().isPlaying){
+					foundTrue = true;
+				}
+				MusicHandler.startLoad = false;
+			}
+
+	        if (isAnalyzed) {
+				if(!executed){
+					home.SetActive(true);
+					Player3D.readyB = true;
+					Score.ready = true;
+					LightChanger.ready = true;
+					changer.ready = true;
+					Touch3D.ready = true;
+					executed = true;
+				}
+				if (!audio.isPlaying) {
+					audio.Play ();
+				}
+	        	UpdateTarget();
+				UpdatePlayer();
+				playerZ += PlayerMovingEasing * (targetPlayerZ - playerZ);
+	        	UpdateCamera();
+				return;
+	        }
+			if(foundTrue){
+				if(!reInstantiate){
+					slider.SetActive(true);
+					analyzer = new MusicAnalyzer(GetComponent<Camera>().GetComponent<AudioSource>().clip, sampleSize, soundFeed, beatSubbands, beatSensitivity, 
+						thresholdSize, thresholdMultiplier);
+					reInstantiate = true;
+				}
+				try{
+	        		if (analyzer.Analyze()) 
+					{
+						if(PlayerPrefs.GetInt ("way", 0) == 0){
+							Screen.orientation = ScreenOrientation.Landscape;
+						} else if (PlayerPrefs.GetInt ("way", 0) == 1){
+							Screen.orientation = ScreenOrientation.Portrait;	
+						}
+						Destroy(menu);
+						health.SetActive(true);
+						coins.SetActive(true);
+						Track track = new Track(trackWidth, trackHeight, trackDepth, Color.black, analyzer.Thresholds);
+              	 	 	GetComponent<Camera>().transform.position = new Vector3(-trackWidth / 2, (float)(analyzer.Thresholds[0] * trackDepth + 100), 0);
+						player.transform.position = new Vector3(-trackWidth / 2, (float)(analyzer.Thresholds[0] * trackDepth + 100), 0);
+	          	  		isAnalyzed = true;
+	        		}
+				} catch(NullReferenceException){
+					
+				}
+			}
+	    }
+
+		double diff;
+		void UpdateCamera()
+		{
+			double d = -((playerZ + cameraDistanceZ) / trackHeight);
+			int c = (int)d;
+
+			if (c > 0)
+			{
+				diff = analyzer.Thresholds[c + 1] - analyzer.Thresholds[c];
+
+				GetComponent<Camera>().transform.position = new Vector3(
+					-trackWidth / 2, 
+					(float)((analyzer.Thresholds[c] +  (d - c) * diff) * trackDepth + cameraDistanceY), 
+					-(float)(playerZ + cameraDistanceZ)
+					);
+			}
+		}
+			
+		float xPos = -512f;
+		KeyCode left = KeyCode.A; KeyCode right = KeyCode.D;
+
+		void UpdatePlayer(){
+			double dPlayer = -(playerZ/trackHeight);
+			int e = (int) dPlayer;
+
+			if (e>0){
+				if (player.transform.position.x == -250){
+					if(Touch3D.left || Input.GetKeyDown(left)){
+						xPos = -512f;
+						Touch3D.left = false;
+					}
+				}
+				if(transform.position.x == -512 ){
+					if(Touch3D.left || Input.GetKeyDown(left)){
+						xPos = -770f;
+						Touch3D.left = false;
+					}
+				}
+				if (player.transform.position.x == -512){
+					if(Touch3D.right || Input.GetKeyDown(right)){
+						xPos = -250f;
+						Touch3D.right = false;
+					}
+				}
+				if (player.transform.position.x == -770){
+					if(Touch3D.right || Input.GetKeyDown(right)){
+						xPos = -512f;
+						Touch3D.right = false;
+					}
+				}
+				player.transform.position = new Vector3(
+					xPos, 
+					(float)((analyzer.Thresholds[e] +  (dPlayer - e) * diff) * trackDepth + 125), 
+					-(float)(playerZ + 100)
+				);
+			}
+		}
+		//double prev;
+
+		void UpdateTarget()
+		{
+			int c = (int)(audio.timeSamples / sampleSize);
+
+			if (c > lastSample && lastSample < analyzer.Thresholds.Length)
+			{
+				try{
+				for (int i = lastSample + 1; i <= c; i++)
+					
+					targetPlayerZ += -analyzer.Thresholds[i] * trackHeight * analyzer.SpeedFactor;
+					lastSample = c;
+				} catch(IndexOutOfRangeException){
+					Screen.orientation = ScreenOrientation.Portrait;	
+					int timeInt = (int)time;
+					PlayerPrefs.SetInt ("time", timeInt);
+					PlayerPrefs.SetInt ("WIN", 1);
+					SceneManager.LoadScene ("GameOver3DE");
+				}
+			}
+
+		}
+	}
+}
---
+++ Track.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace DenisVizigin.Objects3D 
+{ 
+	public class Track : MonoBehaviour
+    {
+        private double[] _fluxThresholds;
+
+        private int _segmentsH;
+        private float _width;
+        private float _depth;
+        private float _height;
+
+        private GameObject _instance = new GameObject();
+
+        public Track(float width, float height, float depth, Color color, double[] fluxThresholds)
+        {
+            _instance.name = "Track";
+            _fluxThresholds = fluxThresholds;
+            _depth = depth;
+            _width = width;
+            _height = (fluxThresholds.Length - 1) * height;
+            _segmentsH = fluxThresholds.Length - 1;
+
+            var meshRenderer = _instance.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+            meshRenderer.material = (Material)Resources.Load("TrackMaterial");
+			_instance.AddComponent<Rigidbody>().isKinematic = true;
+            Build();
+        }
+
+		public static GameObject laser;
+		public static GameObject sphereRed;
+		public static GameObject sphereOrange;
+		public static GameObject sphereYellow;
+		public static GameObject sphereGreen;
+		public static GameObject sphereBlue;
+		public static GameObject spherePurple;
+		public static GameObject ring;
+
+		Vector3 posY;
+		Vector3 posZ;
+		int posCase;
+		int sphereCase;
+		GameObject theeSphere;
+		int laserPosCase;
+		int multiple;
+		int laserMultiple;
+
+        private void Build()
+        {
+            Vector3[] vertices = new Vector3[2 * (_segmentsH + 1)];
+            Vector3[] normals = new Vector3[2 * (_segmentsH + 1)];
+            Vector2[] uvs = new Vector2[2 * (_segmentsH + 1)];
+            int[] triangles = new int[6 * _segmentsH];
+
+            float x, y, z;
+            float ny, nz;
+            float sny, snz, s;
+
+            int numIndices = 0;
+            int index = 0;
+            int basis;
+
+            sny = snz = 0.0f;
+            for (int yi = 0; yi <= _segmentsH; yi++)
+            {
+                for (int xi = 0; xi <= 1; xi++)
+                {
+                    x = (xi - 1) * _width;
+                    y = (float)_fluxThresholds[_segmentsH - yi] * _depth;
+                    z = -((float)yi / (float)_segmentsH - 1) * _height;
+                    vertices[index] = new Vector3(x, y, z);
+
+					if (index != 0 && index % 250 == 0) {
+						Instantiate (ring, new Vector3 (-512, y, z), Quaternion.Euler(90,0,0));
+					}
+					multiple = spawnPosZ();
+					if(index != 0 && index % multiple == 0){
+						posCase = spawnPos();
+						sphereCase = spawnSphere();
+						theeSphere = (sphereCase == 1) ? sphereRed : (sphereCase == 2) ? sphereOrange :
+							(sphereCase == 3) ? sphereYellow : (sphereCase == 4) ? sphereGreen : (sphereCase == 5) ? sphereBlue :
+							(sphereCase == 6) ? spherePurple : null;
+						switch(posCase){
+						case 1: Instantiate(theeSphere, new Vector3(-250, y + 100, z), Quaternion.identity); break;
+						case 2: Instantiate(theeSphere, new Vector3(-512, y + 100, z), Quaternion.identity); break;
+						case 3: Instantiate(theeSphere, new Vector3(-770, y + 100, z), Quaternion.identity); break;
+						}
+					}
+					if(PlayerPrefs.GetInt("easy",0) == 1){
+						if(index != 0 && index % 50 == 0){
+							laserPosCase = laserSpawnX();
+							switch(laserPosCase){
+							case 1: Instantiate(laser, new Vector3(-110, y + 100, z), Quaternion.Euler(0,270,0)); break;
+							case 2: Instantiate(laser, new Vector3(-387, y + 100, z), Quaternion.Euler(0,270,0)); break;
+							case 3: Instantiate(laser, new Vector3(-686, y + 100, z), Quaternion.Euler(0,270,0)); break;
+							}
+						}
+					} else if (PlayerPrefs.GetInt("hard",0) == 1){
+						if(index != 0 && index % 35 == 0){
+							laserPosCase = laserSpawnX();
+							switch(laserPosCase){
+							case 1: Instantiate(laser, new Vector3(-110, y + 100, z), Quaternion.Euler(0,270,0)); break;
+							case 2: Instantiate(laser, new Vector3(-387, y + 100, z), Quaternion.Euler(0,270,0)); break;
+							case 3: Instantiate(laser, new Vector3(-686, y + 100, z), Quaternion.Euler(0,270,0)); break;
+							}
+						}
+					}
+
+                    if (xi == 0)
+                    {
+                        ny = -_width * ((((yi + 1) / _segmentsH - 1) * _height) - y);
+                        nz = (float) ((yi != 0) ? -_width * (_fluxThresholds[_segmentsH - yi + 1] * _depth - _fluxThresholds[_segmentsH - yi] * _depth) : 1);
+
+						s = (float) Math.Sqrt(ny * ny + nz * nz);
+                        sny = Math.Abs(ny / s);
+                        snz = nz / s;
+                    }
+
+                    normals[index] = new Vector3(0, sny, snz);
+
+                    if (xi == 0 && yi != _segmentsH)
+                    {
+                        basis = 2 * yi;
+                        triangles[numIndices++] = (basis + 1);
+                        triangles[numIndices++] = (basis + 2 + 1);
+                        triangles[numIndices++] = basis;
+                        triangles[numIndices++] = (basis + 2 + 1);
+                        triangles[numIndices++] = (basis + 2);
+                        triangles[numIndices++] = basis;
+                    }
+
+                    uvs[index] = new Vector2(
+                         xi * (_width / 512), 
+                         ((float)yi / (float)_segmentsH) * _height / 512
+                         );
+                    index++;
+                }
+            }
+
+            Mesh mesh = new Mesh
+                {
+                    vertices = vertices, 
+                    triangles = triangles,
+                    normals = normals,
+                    uv = uvs
+                };
+            mesh.RecalculateNormals();
+            
+            var meshFilter = (MeshFilter)_instance.AddComponent(typeof(MeshFilter));
+            meshFilter.mesh = mesh;
+        }
+
+		private int spawnPos(){
+			return UnityEngine.Random.Range(1,4);
+		}
+
+		private int spawnPosZ(){
+			int rand;
+			int value;
+			rand = UnityEngine.Random.Range(0,3);
+			return value = (rand == 0) ? 50 : (rand == 1) ? 65 : (rand == 2) ? 90 : 0;
+		}
+
+		private int spawnSphere(){
+			return UnityEngine.Random.Range(1,7);
+		}
+
+		private int laserSpawnX(){
+			return UnityEngine.Random.Range(1,4);
+		}
+    }
+}
