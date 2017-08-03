28046a21b1899fc4018fbbe117c9e3175c2a0b72
Louis Moc <louismc4@hotmail.com>
2017-08-03 17:52:32 -0600

    Fluxes, FFT, Sound Parsing

---
+++ MusicAnalyzer.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DenisVizigin.Sound
+{
+    public class MusicAnalyzer
+    {
+		private float _thresholdMultiplier;
+
+        private double[] _threshold;
+
+        private double[] _peaks;
+		
+        private SoundParser _soundParser;
+		
+		private double _sumOfFluxThresholds;
+
+		private int _thresholdSize;
+
+		int max = 100;
+		float start = 0; 
+		GameObject loadingSlider = Main.slider;
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DenisVizigin.Sound.MusicAnalyzer"/> class.
+		/// MusicAnalyzer creates array of 3d track heights (0..1) analizing sound parameters. 
+		/// </summary>
+		/// <param name="sound">Main audioClip object for analyzing soundwave</param>
+		/// <param name="sampleSize">Size of block in samples (can be 1024, 2048, 4096 etc.)</param>
+		/// <param name="soundFeed">How many blocks analyzed in one Update's method call</param>
+		/// <param name="beatSensitivity">Beat sensitivity of Beat Detector</param>
+		/// <param name="thresholdSize">Threshold size</param>
+		/// <param name="thresholdMultiplier">Threshold multiplier</param>
+
+		void Start(){
+			loadingSlider = GameObject.Find("LoadingSlider");
+			loadingSlider.GetComponent<Slider>().maxValue = max;
+		}
+
+		public MusicAnalyzer(AudioClip sound, 
+		                     int sampleSize = 1024, 
+		                     int soundFeed = 40,
+		                     int beatSubbands = 3, 
+		                     double beatSensitivity = 1.5, 
+		                     int thresholdSize = -1, 
+		                     float thresholdMultiplier = 1.5f)
+		{
+			_thresholdMultiplier = thresholdMultiplier;
+			// THRESHOLD WINDOW SIZE (if -1 then autoSize)
+			_thresholdSize = (thresholdSize < 0) ? (int)(3 * sound.length) : thresholdSize;
+
+			_soundParser = new SoundParser( sound, sampleSize, soundFeed, beatSubbands, beatSensitivity );
+			_threshold = new double[_soundParser.TotalSamples];
+			_peaks = new double[_soundParser.TotalSamples];
+		}
+
+		/// <summary>
+		/// Analyze method calls when MonoBehaviour calls Update method.
+		/// It call main Parse method of the _soundParser object. After parse
+		/// MusicAnalyzer calculate FluxThresholds, smooth it by Kalman's filter
+		/// and convert in 1..0 representation.
+		/// </summary>
+		public bool Analyze()
+		{
+			if (_soundParser.Parse())
+			{
+				CalculateFluxThresholds();
+				DetectPeaks();
+				CalculateKalmanFilter();
+				ConvertPercents();
+				CalculateSumOfThresholds();
+				return true;
+			}
+			else
+			{
+				loadingSlider.GetComponent<Slider>().value = (float)(100 * Math.Round((float)_soundParser.ParseSampleCount / (float)_soundParser.TotalSamples, 2));
+				Debug.Log( "Parsed " + 100 * Math.Round((float)_soundParser.ParseSampleCount / (float)_soundParser.TotalSamples, 2) + "% of sound" );
+				return false;
+			}
+		}
+		
+		/// <summary>
+		/// Calculates the flux thresholds.
+		/// </summary>
+		private void CalculateFluxThresholds()
+		{
+			for( int i = 0; i < _soundParser.TotalSamples; i++ )
+			{
+				int start = Math.Max( 0, i - _thresholdSize / 2 );
+				int end = Math.Min( _soundParser.SpectralFlux.Length - 1, i + _thresholdSize / 2 );
+				double mean = 0;
+				for(int j = start; j <= end; j++ )
+					mean += _soundParser.SpectralFlux[j];
+				mean /= (end - start);
+				_threshold[i] = mean * _thresholdMultiplier;
+			}
+		}
+
+		/// <summary>
+		/// Alternate way to detect peaks.
+		/// </summary>
+		private void DetectPeaks()
+		{
+			double[] prunnedSpectralFlux = new double[_threshold.Length];
+
+			for( int i = 0; i < _threshold.Length; i++ )
+			{
+				if( _threshold[i] <= _soundParser.SpectralFlux[i] )
+				    prunnedSpectralFlux[i] = _soundParser.SpectralFlux[i] - _threshold[i];
+				else
+				    prunnedSpectralFlux[i] = 0;
+			}
+
+			for( int i = 0; i < prunnedSpectralFlux.Length - 1; i++ )
+			{
+			   if( prunnedSpectralFlux[i] > prunnedSpectralFlux[i+1] )
+			      _peaks[i] = prunnedSpectralFlux[i];
+			   else
+			      _peaks[i] = 0;				
+			}
+		}
+
+		/// <summary>
+		/// Kalman's filter for smooth xy function. For our case filter will smooth flux thresholds function.
+		/// More: http://en.wikipedia.org/wiki/Kalman_filter
+		/// </summary>
+		/// <param name="q">Measurement noise</param>
+		/// <param name="r">Environment noise</param>
+		/// <param name="f">Factor of real value to previous real value</param>
+		/// <param name="h">Factor of measured value to real value</param>
+		private void CalculateKalmanFilter(double q = .35, double r = 35, double f = 1, double h = 1)
+		{
+			double state = _threshold[0];
+			double covariance = .1;
+			
+			for (int i = 0; i < _threshold.Length; i++)
+			{
+				double x0 = f * state;
+				double p0 = f * covariance * f + q;
+				double k = h * p0 / (h * p0 * h + r);
+				state = x0 + k * (_threshold[i] - h * x0);
+				covariance = (1 - k * h) * p0;
+				_threshold[i] = state;
+			}
+		}
+
+		/// <summary>
+		/// Convert all flux thresholds values into 0..1 representation.
+		/// </summary>
+		private void ConvertPercents()
+		{
+			double maxFlux = _threshold[0];
+			int i;
+			for (i = 1; i < _threshold.Length; i++)
+				maxFlux = (maxFlux < _threshold[i]) ? _threshold[i] : maxFlux;
+			for (i = 0; i < _threshold.Length; i++)
+				_threshold[i] = _threshold[i] / maxFlux;
+		}
+
+		private void CalculateSumOfThresholds()
+		{
+			_sumOfFluxThresholds = 0;
+			for (int i = 0; i < _threshold.Length; i++)
+				_sumOfFluxThresholds += _threshold[i];
+		}
+
+        public double[] Thresholds { get { return _threshold; } }
+
+        public double[] Peaks { get { return _peaks; } }
+
+        public double[,] Beats { get { return _soundParser.Beats; } }
+
+        public double SpeedFactor { get { return _threshold.Length / _sumOfFluxThresholds; } }
+    }
+}
---
+++ SoundParser.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace DenisVizigin.Sound
+{
+    public class SoundParser
+	{
+		int max = 100;
+		float start = 0; 
+		public Slider loadingSlider;
+		public int HistoryBuffer = 44;
+
+		private double[] _FFTBytes;
+		private double[] _lastFFTBytes;
+		
+		private int[] _detectedBeats;
+		private double[] _spectralFlux;
+		private double[] _rawBytes;
+		private double[,] _beats;
+
+		private List<List<double>> _energyHistoryBuffer;
+		
+		private AudioClip _sound;
+		private int _totalSamples;
+		private int _totalBeats = 0;
+		private int _parseSampleCount = 0;
+
+		private double _beatSensitivity;
+		private int _beatSubbands;
+		private int _sampleSize;
+		private int _soundFeed;
+
+
+		void Start(){
+			loadingSlider = GameObject.Find("LoadingSlider").GetComponent<Slider>();
+			loadingSlider.maxValue = max;
+		}
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DenisVizigin.Sound.SoundParser"/> class.
+		/// </summary>
+		/// <param name="sound">Main audioClip object for analyzing soundwave</param>
+		/// <param name="sampleSize">Size of block in samples (can be 1024, 2048, 4096 etc.)</param>
+		/// <param name="soundFeed">How many blocks analyzed in one Update's method call</param>
+		/// <param name="beatSensitivity">Beat sensitivity</param>
+		public SoundParser(AudioClip sound, int sampleSize, int soundFeed, int beatSubbands, double beatSensitivity)
+		{
+			_sound = sound;
+			_soundFeed = soundFeed;
+			_sampleSize = sampleSize;
+			_beatSubbands = beatSubbands;
+			_beatSensitivity = beatSensitivity;
+
+			_totalSamples = (int)(sound.samples / _sampleSize);
+			_rawBytes = new double[_sampleSize];
+			_FFTBytes = new double[_sampleSize / 2];
+			_lastFFTBytes = new double[_sampleSize / 2];
+			
+			_spectralFlux = new double[TotalSamples];
+
+			_beats = new double[TotalSamples, beatSubbands];
+			_detectedBeats = new int[TotalSamples];
+			_energyHistoryBuffer = new List<List<double>>();
+		}
+
+		/// <summary>
+		/// Just section's parse mechanics with calling sound analyzing
+		/// method WriteData in loop
+		/// </summary>
+		public bool Parse()
+		{
+			if (_parseSampleCount >= TotalSamples)
+				return true;
+			
+			for (int j = 0; j < _soundFeed; j++)
+			{
+				if (_parseSampleCount + j >= TotalSamples) 
+					break;
+				WriteData(_parseSampleCount + j);
+			}
+
+			_parseSampleCount = (_parseSampleCount + _soundFeed > TotalSamples) ? TotalSamples : _parseSampleCount + _soundFeed;
+			return false;
+		}
+
+		/// <summary>
+		/// Analyzing sound function make in 5 stages:
+		/// 1. Get sound data by GetData method;
+		/// 2. Run Hamming window for smoothing;
+		/// 3. Run FTT for calculate magnitutes;
+		/// 4. Calculate beats from this algoritm: http://archive.gamedev.net/archive/reference/programming/features/beatdetection/;
+		/// 5. Calculate spectral flux; 
+		/// </summary>
+		/// <param name="currentSample">Current sample.</param>
+		private void WriteData(int currentSample)
+		{
+			int startPosition = currentSample * _sampleSize;
+            float[] samples = new float[_sampleSize * _sound.channels]; 
+            _sound.GetData(samples, startPosition);
+
+			if (_sound.channels == 1)
+			{
+				for (int i = 0; i < samples.Length; i++)
+		        	_rawBytes[i] = samples[i];
+		    }
+		    else
+		    {
+		    	for (int i = 0; i < samples.Length; i = i + 2) {
+		        	_rawBytes[i / 2] = .5 * (samples[i] + samples[i+1]);
+		    	}
+		    }
+
+			RunHammingWindow();
+			RunFFT(10, false);
+			_spectralFlux[currentSample] = (currentSample < 1) ? 0 : CalculateSpectralFlux();
+			CalculateBeats(currentSample);
+
+			// Just copy built-in arrays
+			for (int i = 0; i < _FFTBytes.Length; i++)
+		       	_lastFFTBytes[i] = _FFTBytes[i];
+		}
+
+		/// <summary>
+		/// Method smoothing raw bytes by hamming function
+		/// More: http://en.wikipedia.org/wiki/Window_function#Generalized_Hamming_windows
+		/// </summary>
+        private void RunHammingWindow()
+		{
+			for (int i = 0; i < _rawBytes.Length; i++)
+				_rawBytes[i] = (0.54 + 0.46 * Math.Cos(2 * Math.PI * i / _rawBytes.Length)) * _rawBytes[i];
+		}
+
+		/// <summary>
+		/// Run FFT function and then compute magnitutes of real and imagnary parts of sound.
+		/// </summary>
+		/// <param name="logN">Log2 of FFT length. e.g. for 512 pt FFT, logN = 9</param>
+		/// <param name="inverse">If set to <c>true</c> inverse.</param>
+		private void RunFFT(uint logN, bool inverse = false)
+		{
+			double[] re = new double[_rawBytes.Length];
+			double[] im = new double[_rawBytes.Length];
+
+			for (int j = 0; j < _rawBytes.Length; j++)
+			{
+				re[j] = _rawBytes[j];
+				im[j] = 0;
+			}
+			
+			FFT fft = new FFT();
+			fft.init(logN);
+			fft.run(re, im, inverse);
+
+			for (int i = 0; i < _FFTBytes.Length; i++)
+				_FFTBytes[i] = Math.Sqrt(re[i] * re[i] + im[i] * im[i]);
+		}
+
+		/// <summary>
+		/// Calculates the spectral flux
+		/// </summary>
+		/// <returns>The spectral flux</returns>
+       	private double CalculateSpectralFlux()
+		{
+			double diff = 0;
+			double flux = 0;
+			
+			for (int i = 0; i < _FFTBytes.Length; i++)
+			{	
+				diff = _FFTBytes[i] - _lastFFTBytes[i];
+				flux += ((diff < 0) ? 0 : diff);
+			}
+
+			return flux;
+		}
+
+		/// <summary>
+		/// Beat Detector
+		/// Calculates the beats by this algoritm: http://archive.gamedev.net/archive/reference/programming/features/beatdetection/;
+		/// It algoritm used in all libs but it's not so good. Better use onset detection
+		/// </summary>
+		/// <param name="fftBytes">fft bytes</param>
+		/// <param name="currentSample">Current analyzing sample</param>
+		private void CalculateBeats(int currentSample) 
+		{
+			double[] es = new double[_beatSubbands];
+			int detectedBeats = 0;
+			int i = 0;
+
+			// init beats on the line (zero = no beats)
+			for (i = 0; i < _beatSubbands; i++) {
+				_detectedBeats [currentSample] = 0;
+				_beats [currentSample, i] = es [i] = 0.0;
+			}
+
+			// divide all bytes into _beatSubbands parts
+			for (i = 0; i < _FFTBytes.Length; i++)
+			{
+				int c = (int)((float)i / ((float)_FFTBytes.Length / (float)_beatSubbands));
+				es[c] += _FFTBytes[i];
+			}
+
+			if (currentSample % HistoryBuffer == 0)
+			{
+				if (_energyHistoryBuffer.Count == HistoryBuffer)
+				{
+					for (i = 0; i < es.Length; i++)
+					{
+						if (es[i] > _beatSensitivity * ComputeAverageHistoryEnergy(i))
+						{
+							_beats[currentSample,i] = es[i];
+							_totalBeats++;
+							detectedBeats++;
+						}
+					}
+
+					_detectedBeats[currentSample] = detectedBeats;
+				}
+			}
+			
+			AddHistoryEnergy(es);
+		}
+
+		/// <summary>
+		/// Computes the average history energy
+		/// </summary>
+		/// <returns>The average history energy</returns>
+		/// <param name="subband">Subband</param>
+		private double ComputeAverageHistoryEnergy(int subband)
+		{
+			double averageEnergy = 0;
+			for (int i = 0; i < _energyHistoryBuffer.Count; i++)
+				averageEnergy += _energyHistoryBuffer[i][subband];
+			return averageEnergy / (double)_energyHistoryBuffer.Count;
+		}
+
+		/// <summary>
+		/// Method control energy history buffer size (HistoryBuffer) by queue algoritm and add new energy value into list
+		/// </summary>
+		/// <param name="es">Energy value</param>
+		private void AddHistoryEnergy(double[] es)
+		{
+			if (_energyHistoryBuffer.Count >= HistoryBuffer)
+				_energyHistoryBuffer.RemoveAt(0);
+
+			for (int i = 0; i < es.Length; i++) {
+				if (i == 0) _energyHistoryBuffer.Add( new List<double>(_beatSubbands) );
+				_energyHistoryBuffer[_energyHistoryBuffer.Count - 1].Add(es[i]);
+			}
+		}
+		
+		public int ParseSampleCount { get { return _parseSampleCount; } }
+		
+		public int TotalSamples { get { return _totalSamples; } }
+		
+		public double[] SpectralFlux { get { return _spectralFlux; } }
+
+		public double[,] Beats { get { return _beats; } }
+		
+		public int[] DetectedBeats { get { return _detectedBeats; } }
+
+		public int TotalBeats { get { return _totalBeats; } }
+    }
+}
